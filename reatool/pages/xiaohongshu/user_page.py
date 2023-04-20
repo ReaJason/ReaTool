fbbcb568a86e4e22599af4313eed67c0c7273f13
ReaJason <reajason1225@gmail.com>
2023-04-20 10:49:14 +0800

    feat: add pay info

--- reatool/pages/xiaohongshu/user_page.py
+++ reatool/pages/xiaohongshu/user_page.py
@@ -1,7 +1,7 @@
 import time
 
 from PySide6.QtCore import Qt, Slot, Signal, QSize, QThread
-from PySide6.QtGui import QStandardItemModel, QStandardItem, QResizeEvent, QFont
+from PySide6.QtGui import QStandardItemModel, QStandardItem, QResizeEvent, QFont, QPixmap
 from PySide6.QtWidgets import QFrame, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QTableView, QTabBar, QTabWidget
 
 from reatool.core import GetSelfUserThread
@@ -173,7 +173,23 @@ class CrawlAbout(QFrame):
                 <li>邮箱📮：<a href='mailto:reajason1225@gmail.com'>reajason1225@gmail.com</a></li>
                 <li>GitHub🎉：<a href='https://github.com/ReaJason'>ReaJason</a></li>
             </ul>
-            <h3>免责声明</h3>
+            <h3>Buy me a coffee</h3>
+            如果觉得这个小工具有帮到您的话，欢迎打赏一杯奶茶 ☕️ 
+        </center>
+        """)
+        about_info.setOpenExternalLinks(True)
+        pay_layout = QHBoxLayout()
+        pay_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        wechat_pay = QLabel()
+        wechat_pay.setPixmap(QPixmap("asserts/wechat.png").scaledToWidth(150))
+        ali_pay = QLabel()
+        ali_pay.setPixmap(QPixmap("asserts/alipay.jpg").scaledToWidth(140))
+        pay_layout.addWidget(wechat_pay)
+        pay_layout.addWidget(ali_pay)
+        layout.addWidget(about_info)
+        layout.addLayout(pay_layout)
+        layout.addWidget(QLabel("""
+        <h3>免责声明</h3>
             <ol>
               <li>本软件采集到的内容均可在网页上获取到，所有内容版权归原作者所有。</li>
               <li>本软件提供的所有资源，仅可用于学习交流使用，未经原作者授权，禁止用于其他用途。</li>
@@ -181,10 +197,6 @@ class CrawlAbout(QFrame):
               <li>任何涉及商业盈利目的均不得使用，否则一些后果由您承担</li>
               <li>因使用本软件产生的版权问题，软件作者概不负责</li>
             </ol>
-        </center>
-        
-        """)
-        about_info.setOpenExternalLinks(True)
-        layout.addWidget(about_info)
+        """))
         self.setLayout(layout)
         self.setStyleSheet("""border: none;margin:0;""")
