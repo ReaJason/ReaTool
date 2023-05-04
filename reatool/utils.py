1c4d90d57d64c5c3d8c737fb4ce2f757f579f884
ReaJason <reajason1225@gmail.com>
2023-05-04 13:38:05 +0800

    feat: support white space file path

--- reatool/utils.py
+++ reatool/utils.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QMessageBox
 
 
 def open_directory(dir_path):
-    command = rf'start {dir_path}\\'
+    command = rf'explorer "{dir_path}"'
     logging.info(f"当前打开文件路径：{dir_path}，命令为：{command}")
     subprocess.run(command, shell=True)
 
